fddb7f6d8a182211bc174e931fd7cfdb6bc281dc
Giovanni <gcgbarros@gmail.com>
2026-06-24 16:53:02 -0300

    Instalação do NEXTJS e dependências para esse projeto

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-2 text-center">
+      <h1 className="text-2xl font-bold">Projeto Integrador Full-Stack 04</h1>
+      <p className="text-lg text-gray-600">Sistema de Controle de Estoque</p>
+      <p className="mt-4 text-sm font-medium text-green-600">
+        Servidor rodando com sucesso 🟢
+      </p>
+    </main>
+  );
+}
